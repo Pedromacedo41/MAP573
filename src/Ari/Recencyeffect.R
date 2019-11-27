ecbcf706428487482cbebcbdf61161bab50d5514
ari.johannesson <arijoh94@gmail.com>
2019-11-27 19:27:06 +0100

    Lag - clear effects
    Lags effect on zones 4, 8 and 12 more than others.
    Every 12 lags, load zones and temperatures are more correlated

--- src/Ari/Recencyeffect.R
+++ src/Ari/Recencyeffect.R
@@ -10,27 +10,31 @@ TS = subset(TS, select = -c(X, datetime, V10))  #Remove Z10 which is V11 because
 TS_sum <- rowSums(TS[(1:dim(TS)[1]), ])
 TST_mean <- rowMeans(TST[(1:dim(TS)[1]), ])
 
-n <- 10000
+n <- 15000
 TST_mean <- as.matrix(TST_mean[1:n])
 TS_sum <- as.matrix(TS_sum[1:n])
 
 
 counter <- 1
-Imax <- 24*7
-C <- vector(length = Imax+1)
+Imax <- 24*2
+C <- matrix(data=NA, nrow = Imax+1, ncol = 2)
 for (i in (0:(length(C)-1))){
   
   TS_sum_lagged <- as.matrix(TS_sum[(i+1):length(TS_sum)])
   TST_mean_lagged <- as.matrix(TST_mean[1:(length(TST_mean)-i)])
   
-  C[counter] <- cor(TST_mean_lagged, TS_sum_lagged)
+  C[counter,1] <- i
+  C[counter,2] <- cor(TST_mean_lagged, TS_sum_lagged)
   counter = counter + 1
 }
 
-plot(C, main = "Correlation betw. temperature and Load", ylav="Correlation", xlab = "Lag")
-print("Most correlation is at lag:")
-which(min(C)==C)-1 #because index 1 is lag 0
-
+C <- as.data.frame(C)
+colnames(C) <- c("Lag", "Correlation")
+library(ggplot2)
+ggplot(C, aes(x=Lag, y=Correlation)) +
+      theme_bw() + 
+      geom_point() +
+      labs(title="Correlation betw. temperature and load")
 
 
 
