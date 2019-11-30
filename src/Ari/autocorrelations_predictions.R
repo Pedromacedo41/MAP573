ed6c472d394007ef3f229a1bb183a51d1868e07f
ari.johannesson <arijoh94@gmail.com>
2019-11-30 16:28:55 +0100

    autocorrealtion results

--- src/Ari/autocorrelations_predictions.R
+++ src/Ari/autocorrelations_predictions.R
@@ -2,10 +2,14 @@ library(forecast)
 library(ggplot2)
 
 #set current directory to code source
-p <- read.csv("./../../Data/Predict & Hyper/bidirectional_longer.csv", header = F)
+p <- read.csv("./../../Data/Predict & Hyper/ari1.csv", header = F)
 p = p[!is.na(p)]
 
 
 
-ggAcf(p) + labs(title="Autocorrelations for ....")+theme_bw()
-ggAcf(p, type = "partial") + labs(title="Autocorrelations for ....")+theme_bw()
+p1 <- ggAcf(p) + labs(title="Autocorrelation of predicted values")+theme_bw()
+p2 <- ggAcf(p, type = "partial") + labs(title="Partial autocorrelation of predicted values")+theme_bw()
+grid.arrange(p1, p2, nrow = 2)
+
+
+
